7b3d9d57d4d08073dee5d793cb59b5ff3340337e
Peter Drake <drake@lclark.edu>
2013-06-17 14:04:39 -0700

    Die can now roll itself.

--- src/Die.java
+++ src/Die.java
@@ -1,3 +1,13 @@
 public class Die {
 
+	private int topFace;
+	
+	public void roll() {
+		topFace = (int)(Math.random() * 6);
+	}
+	
+	public int getTopFace() {
+		return topFace;
+	}
+
 }
---
+++ src/DieTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DieTest {
+
+	private Die die;
+	
+	@Before
+	public void setUp() throws Exception {
+		die = new Die();
+	}
+
+	@Test
+	public void testRoll() {
+		int[] counts = new int[6];
+		for (int i = 0; i < 6000; i++) {
+			die.roll();
+			counts[die.getTopFace()]++;
+		}
+		for (int i = 0; i < counts.length; i++) {
+			assertTrue(counts[i] >= 750);
+		}
+	}
+
+}
