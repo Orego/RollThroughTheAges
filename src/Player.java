2545ed8187e814a60efceca1bcb565b43b1dfafa
Laura Vonessen <lvonessen@maclabcs19.lclark.edu>
2013-06-18 16:16:09 -0700

    Players can now add goods correctly.

--- src/Player.java
+++ src/Player.java
@@ -2,14 +2,17 @@ import java.util.ArrayList;
 
 public class Player {
 	private City[] cities = new City[7];
-//	private ArrayList<Development> developments;
+	private PlayerResources resources = new PlayerResources();
+
+	// private ArrayList<Development> developments;
 
 	public Player() {
 		initializeCities();
 	}
-	
-	/** Set each player starts with 7 total cities with 3 full and 4 
-	 *  with the correct number of max workers
+
+	/**
+	 * Set each player starts with 7 total cities with 3 full and 4 with the
+	 * correct number of max workers
 	 */
 	private void initializeCities() {
 		cities[0] = new City(0);
@@ -20,8 +23,8 @@ public class Player {
 		cities[5] = new City(5);
 		cities[6] = new City(6);
 	}
-	
-	/** Returns the number of full cities*/
+
+	/** Returns the number of full cities */
 	public int getNumCities() {
 		int count = 0;
 		for (City c : cities) {
@@ -31,13 +34,55 @@ public class Player {
 		}
 		return count;
 	}
-	
-	/** Return the city with this index*/
+
+	/** Return the city with this index */
 	public City getCity(int i) {
 		return cities[i];
 	}
-	
+
 	public void buyCityWorkers(int workers, int city) {
 		cities[city].addWorkers(workers);
+	}
+
+	/** Add the specified number of goods to the player's resources. */
+	public void addGoods(int numGoods) {
+		int skipCounter = 0, goodTypeCounter = 0, returnValue = 0;
+		while (numGoods > 0 && skipCounter < 5) {
+			returnValue = resources.changeAmount(goodTypeCounter % 5, 1);
+			if (returnValue == 0) {
+				numGoods--;
+				skipCounter = 0;
+			} else
+				skipCounter++;
+			goodTypeCounter++;
+		}
+	}
+
+	/** Get a visual representation of the player's resources. */
+	public String getResourcesInfo() {
+		return "Resources:\nSpearheads: "
+				+ resources.getAmount(PlayerResources.SPEARHEADS) + "\nCloth: "
+				+ resources.getAmount(PlayerResources.CLOTH) + "\nPottery: "
+				+ resources.getAmount(PlayerResources.POTTERY) + "\nStone: "
+				+ resources.getAmount(PlayerResources.STONE) + "\nWood: "
+				+ resources.getAmount(PlayerResources.WOOD) + "\nFood: "
+				+ resources.getAmount(PlayerResources.FOOD);
+	}
+	
+	public static void main(String[] args) {
+		Player p = new Player();
+		System.out.println(p.getResourcesInfo());
+		p.addGoods(3);
+		System.out.println("Added 3 goods.");
+		System.out.println(p.getResourcesInfo());
+		p.addGoods(7);
+		System.out.println("Added 7 goods.");
+		System.out.println(p.getResourcesInfo());
+		p.addGoods(13);
+		System.out.println("Added 13 goods.");
+		System.out.println(p.getResourcesInfo());
+		p.addGoods(10);
+		System.out.println("Added 10 goods.");
+		System.out.println(p.getResourcesInfo());
 	}
 }
--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -23,5 +23,20 @@ public class PlayerTest {
 		System.out.println("Did we successfully add 3 workers to city #4? " + p.getCity(3).addWorkers(3));
 		System.out.println("Is City #4 full? " + p.getCity(3).isFull());
 	}
+	
+	@Test
+	public void testAddGoods() {
+		assertEquals("Resources:\nSpearheads: 0\nCloth: 0\nPottery: 0\nStone: 0\nWood: 0\nFood: 3",p.getResourcesInfo());
+		p.addGoods(0);
+		assertEquals("Resources:\nSpearheads: 0\nCloth: 0\nPottery: 0\nStone: 0\nWood: 0\nFood: 3",p.getResourcesInfo());
+		p.addGoods(3);
+		assertEquals("Resources:\nSpearheads: 0\nCloth: 0\nPottery: 1\nStone: 1\nWood: 1\nFood: 3",p.getResourcesInfo());
+		p.addGoods(7);
+		assertEquals("Resources:\nSpearheads: 1\nCloth: 1\nPottery: 2\nStone: 3\nWood: 3\nFood: 3",p.getResourcesInfo());
+		p.addGoods(13);
+		assertEquals("Resources:\nSpearheads: 3\nCloth: 3\nPottery: 5\nStone: 6\nWood: 6\nFood: 3",p.getResourcesInfo());
+		p.addGoods(10);
+		assertEquals("Resources:\nSpearheads: 4\nCloth: 5\nPottery: 6\nStone: 7\nWood: 8\nFood: 3",p.getResourcesInfo());
+	}
 
 }
