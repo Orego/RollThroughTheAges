1e4c47bbf3cceaa958374c44225dfffe3da2a29a
Laura Vonessen <lvonessen@maclabcs19.lclark.edu>
2013-06-19 13:32:06 -0700

    Added a button to verify that the update method works.

--- src/views/ResourcesPanel.java
+++ src/views/ResourcesPanel.java
@@ -1,18 +1,23 @@
 package views;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import models.PlayerResources;
-
+/** This class displays resources graphically. */
 public class ResourcesPanel extends JPanel {
 
+	/** The images associated with the different resources. */
 	private static final ImageIcon[] RESOURCE_IMAGES = {
 			new ImageIcon("Images/wood.jpg"),
 			new ImageIcon("Images/stone.jpg"),
@@ -21,9 +26,14 @@ public class ResourcesPanel extends JPanel {
 			new ImageIcon("Images/spearhead.jpg"),
 			new ImageIcon("Images/food2.jpg") };
 
+	/** These arrays display the amount and worth of goods in each category. */
 	private JLabel[] amountLabel, worthLabel;
 
-	private JLabel totalAmount, totalWorth;
+	/** The total amount of goods. */
+	private JLabel totalAmount;
+	
+	/** The worth of total goods. */
+	private JLabel totalWorth;
 
 	public ResourcesPanel() {
 		amountLabel = new JLabel[6];
@@ -31,6 +41,7 @@ public class ResourcesPanel extends JPanel {
 		layoutGUI();
 	}
 
+	/** Lays out the components of the panel. */
 	private void layoutGUI() {
 		this.setLayout(new GridLayout(0, 3, 3, 3));// rows, columns
 		setPreferredSize(new Dimension(60 * 3, 40 * 7));
@@ -56,23 +67,9 @@ public class ResourcesPanel extends JPanel {
 		this.add(amountLabel[PlayerResources.FOOD]);
 		worthLabel[PlayerResources.FOOD] = new JLabel();
 		this.add(worthLabel[PlayerResources.FOOD]);
-
 	}
 
-	// private class JLabel2 extends JLabel{
-	// private JLabel2(){
-	// super();
-	// setup();
-	// }
-	//
-	// private JLabel2(String text){
-	// super(text);
-	// setup();
-	// }
-	//
-	//
-	// }
-
+	/** Updates the amount and worth of resources. */
 	public void updateResources(int[] amount, int[] worth, boolean granaries) {
 		int totalAmount = 0, totalWorth = 0;
 		for (int i = 0; i < 6; i++) {
@@ -90,11 +87,35 @@ public class ResourcesPanel extends JPanel {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.add(new ResourcesPanel());
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setPreferredSize(new Dimension(60 * 3, 40 * 8));
+		ResourcesPanel rp = new ResourcesPanel();
+		panel.add(rp, BorderLayout.CENTER);
+		JButton changeAndUpdate = new JButton("+1 resource to each");
+		changeAndUpdate.addActionListener(rp.new ButtonListener());
+		panel.add(changeAndUpdate,BorderLayout.SOUTH);
+		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private class ButtonListener implements ActionListener {
+		private int clickCounter;
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			clickCounter++;
+			int temp = clickCounter*clickCounter+clickCounter;
+			temp/=2;
+			updateResources(
+					new int[] { clickCounter, clickCounter, clickCounter,
+							clickCounter, clickCounter, 3 + clickCounter },
+					new int[] { temp,temp*2,temp*3,temp*4,temp*5,clickCounter*4}, false);
+		}
+
 	}
 
 }
