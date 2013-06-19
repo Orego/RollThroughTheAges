98932f42df463d950e197b7b5a70481cfe4d6cdc
Alex Terenin <aterenin@umail.ucsb.edu>
2013-06-19 10:41:14 -0700

    fixed GUI

--- src/models/Game.java
+++ src/models/Game.java
@@ -1,26 +1,78 @@
 package models;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-
+/** Contains  all the information about the state of the game */
 public class Game {
+	
+	/** holds the list of players */
 	private ArrayList<Player> players;
 	
+	/** holds whose turn it is */
+	private int currentplayer;
+	
+	/** Initializes the game */
 	public Game(int n) {
-		
+		//create players
 		players = new ArrayList<Player>();
 		for(int i = 0; i < n; i++) {
 			players.add(new Player());
 		}
+		
+		
 	}
 	
+	/** Gets the player at the given index */
 	// takes arguments i=1,2,3,4
 	public Player getPlayer(int i) {
 		return players.get(i-1);
 	}
 	
+	/** Get the total number of players in the game */
 	public int getNumPlayers() {
 		return players.size();
+	}
+	
+	/** Return who's turn is it?*/
+	public int getCurrentPlayer(){
+		return currentplayer; 
+	}
+	
+	/** Ends the current turn & starts next one */
+	public void nextTurn(){
+		currentplayer = (currentplayer + 1)%(players.size());
+	}
+	
+	/** Returns a players dice list */
+	public List<Die> getPlayersDice(int player){
+		return players.get(player).getPlayersDice();
+	}
+	
+	/**
+	 * Do the first roll for a player
+	 * @param player the index of the player
+	 * @return the list of dice
+	 */
+	public List<Die> rollPlayersDice(int player){
+		players.get(player).doFirstRoll();
+		return players.get(player).getPlayersDice();
+	}
+	
+	/** 
+	 * Rerolls the players dice
+	 * @param player the index of the player
+	 * @param indices the list of indices of the dice
+	 * @return the list of dice
+	 */
+	public List<Die> rerollPlayersDice(int player,List<Integer> indices){
+		players.get(player).rerollDice(indices);
+		return players.get(player).getPlayersDice();
+	}
+	
+	/** Returns the number of rerolls a player has left */
+	public int getPlayersNumRerolls(int player){
+		return players.get(player).getRerollsLeft();
 	}
 	
 	public static void main(String[] args){
--- src/models/Player.java
+++ src/models/Player.java
@@ -1,12 +1,27 @@
 package models;
 import java.util.ArrayList;
+import java.util.List;
 
+/** Holds information about a player */
 public class Player {
+	
+	/** Holds the list of cities. */
 	private City[] cities = new City[7];
 //	private ArrayList<Development> developments;
+	
+	/** Holds the list of dice that the player is allowed to use */
+	private ArrayList<Die> dice;
+	
+	private RollTracker roller;
 
+	/** Initializes the players */
 	public Player() {
 		initializeCities();
+		dice = new ArrayList<Die>();
+		for (int i = 0; i < 3; i++){
+			dice.add(new Die());
+		}
+		roller = new RollTracker();
 	}
 	
 	/** Set each player starts with 7 total cities with 3 full and 4 
@@ -20,6 +35,43 @@ public class Player {
 		cities[4] = new City(4);
 		cities[5] = new City(5);
 		cities[6] = new City(6);
+	}
+	
+	/** Returns the list of dice that the player can roll */
+	public List<Die> getPlayersDice(){
+		return dice;
+	}
+	
+	/** Does the first roll of the players turn */
+	public void doFirstRoll(){
+		roller.rollDice(dice);
+	}
+	
+	/**
+	 * Reroll the dice
+	 * 
+	 * @param diceToRoll the indices of the dice
+	 * @return true if reroll was possible.
+	 */
+	public boolean rerollDice(List<Integer> diceToRoll){
+		return roller.reroll(diceToRoll, dice);
+	}
+	
+	/** Get the rerolls left */
+	public int getRerollsLeft(){
+		return roller.getRerollsLeft();
+	}
+	
+	/** Adds a die to the players dice list */
+	public void addDie() throws AddedTooManyDiceException {
+		if (dice.size() >= 7) throw new AddedTooManyDiceException();
+		else dice.add(new Die());
+	}
+	
+	/** Thrown when program tries to add more than 7 dice */
+	private class AddedTooManyDiceException extends RuntimeException{
+		
+		private static final long serialVersionUID = 1L;
 	}
 	
 	/** Returns the number of full cities*/
@@ -38,6 +90,13 @@ public class Player {
 		return cities[i];
 	}
 	
+	/** 
+	 * Adds workers to a particular city
+	 * NOTE: needs work....
+	 * 
+	 * @param workers the amount of workers added
+	 * @param city the city number
+	 */
 	public void buyCityWorkers(int workers, int city) {
 		cities[city].addWorkers(workers);
 	}
