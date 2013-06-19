cade05b29978cbf1aafeab3b095cc8f60b56a649
Laura Vonessen <lvonessen@maclabcs19.lclark.edu>
2013-06-19 09:26:46 -0700

    Made test main and updated development tests.

--- src/DevelopmentListTest.java
+++ src/DevelopmentListTest.java
@@ -14,21 +14,10 @@ public class DevelopmentListTest {
 	}
 	@Test
 	public void test() {
-		assertTrue(d.isDevelopmentBought(DevelopmentList.RELIGION)==0);
+		assertFalse(d.isDevelopmentBought(DevelopmentList.RELIGION));
 		d.buyDevelopment(DevelopmentList.COINAGE);
-		assertTrue(d.isDevelopmentBought(DevelopmentList.COINAGE)==1);
-		System.out.println(
-				DevelopmentList.DEVELOPMENT_NAMES[DevelopmentList.COINAGE] +
-				" is number " +
-				DevelopmentList.COINAGE +
-				" and cost is " + 
-				DevelopmentList.DEVELOPMENT_COSTS[DevelopmentList.COINAGE] +
-				" and points are " + 
-				DevelopmentList.DEVELOPMENT_POINTS[DevelopmentList.COINAGE]
-		);
-		System.out.println(
-			d.getAvailableDevelopments() + " coinage is 5, and should not be in this list"
-		);
+		assertTrue(d.isDevelopmentBought(DevelopmentList.COINAGE));
+		assertFalse(d.getAvailableDevelopments().contains(new Integer(DevelopmentList.COINAGE)));
 	}
 
 }
