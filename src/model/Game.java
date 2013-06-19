1d1813d315ced33f48fdf9dadd55281071c913a8
Alex Terenin <aterenin@umail.ucsb.edu>
2013-06-19 15:33:16 -0700

    turn sequence

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,115 +1,143 @@
 package model;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/** This class keeps track of game states and coordinates interactions among classes. */
+/**
+ * This class keeps track of game states and coordinates interactions among
+ * classes.
+ */
 public class Game {
-	
+
 	/** The list of players. */
 	private ArrayList<Player> players;
-	
+
 	/** holds whose turn it is */
 	private int currentplayer;
-	
+
+	/** holds current position */
+	private int current_turn_part;
+
+	public static final String[] TURN_PARTS = {
+			"Roll dice and collect goods and food",
+			"Feed cities revolve disasters",
+			"Build cities and / or monumentes", "Option to buy 1 development",
+			"Discard goods in excess of 6" };
+
 	/**
 	 * 
-	 * @param playerNames An array of player names
+	 * @param playerNames
+	 *            An array of player names
 	 */
 	public Game(String[] playerNames) {
-		
+
 		players = new ArrayList<Player>();
-		for(int i = 0; i < playerNames.length; i++) {
-			players.add(new Player(playerNames[i].trim(),playerNames.length));
+		for (int i = 0; i < playerNames.length; i++) {
+			players.add(new Player(playerNames[i].trim(), playerNames.length));
 		}
-		
-		
+
 	}
-	
+
 	/** Returns information about players 1,2,3, or 4. */
 	public Player getPlayer(int i) {
-		return players.get(i-1);
+		return players.get(i - 1);
 	}
-	
 
 	/** Get the total number of players in the game */
 	public int getNumPlayers() {
 		return players.size();
 	}
-	
-	/** Return who's turn is it?*/
-	public int getCurrentPlayer(){
-		return currentplayer; 
+
+	/** Return who's turn is it? */
+	public int getCurrentPlayer() {
+		return currentplayer;
 	}
 	
-	/** Ends the current turn & starts next one */
-	public void nextTurn(){
-		currentplayer = (currentplayer + 1)%(players.size());
+	/** Return's the part of the turn the player is on */
+	public int getCurrentTurnPart(){
+		return current_turn_part;
 	}
 
-	public DevelopmentList getPlayersDevelopmentList(int player){
+	/** Ends the current turn & starts next one */
+	public void nextTurn() {
+		currentplayer = (currentplayer + 1) % (players.size());
+	}
+	
+	public void nextTurnPart() {
+		if(current_turn_part == 4)
+			nextTurn();
+		current_turn_part = (current_turn_part + 1) % 5;		
+	}
+
+	public DevelopmentList getPlayersDevelopmentList(int player) {
 		return players.get(player).getDevelopementList();
 	}
-	
+
 	/** Returns a players dice list */
-	public List<Die> getPlayersDice(int player){
+	public List<Die> getPlayersDice(int player) {
 		return players.get(player).getPlayersDice();
 	}
-	
-	
+
 	/**
 	 * Do the first roll for a player
-	 * @param player the index of the player
+	 * 
+	 * @param player
+	 *            the index of the player
 	 * @return the list of dice
 	 */
-	public List<Die> rollPlayersDice(int player){
+	public List<Die> rollPlayersDice(int player) {
 		players.get(player).doFirstRoll();
 		return players.get(player).getPlayersDice();
 	}
-	
-	/** 
+
+	/**
 	 * Rerolls the players dice
-	 * @param player the index of the player
-	 * @param indices the list of indices of the dice
+	 * 
+	 * @param player
+	 *            the index of the player
+	 * @param indices
+	 *            the list of indices of the dice
 	 * @return the list of dice
 	 */
-	public List<Die> rerollPlayersDice(int player,List<Integer> indices){
+	public List<Die> rerollPlayersDice(int player, List<Integer> indices) {
 		players.get(player).rerollDice(indices);
 		return players.get(player).getPlayersDice();
 	}
-	
+
 	/** Returns the number of rerolls a player has left */
-	public int getPlayersNumRerolls(int player){
+	public int getPlayersNumRerolls(int player) {
 		return players.get(player).getRerollsLeft();
 	}
-	
+
 	/** Returns the player names in a string separated by commas. */
-	public String getPlayerNames(){
+	public String getPlayerNames() {
 		String str = "";
-		for (int i = 0; i<players.size()-1; i++){
-			str+=players.get(i).getName()+", ";
+		for (int i = 0; i < players.size() - 1; i++) {
+			str += players.get(i).getName() + ", ";
 		}
-		str += players.get(players.size()-1).getName();
+		str += players.get(players.size() - 1).getName();
 		return str;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		System.out.println("choose number of players [1-4]: ");
 		Scanner scan = new Scanner(System.in);
 		int numPlayers = scan.nextInt();
-		while (numPlayers<1 || numPlayers>4) {
+		while (numPlayers < 1 || numPlayers > 4) {
 			System.out.println("out of bounds please choose a number from 1-4");
 			numPlayers = scan.nextInt();
 		}
 		scan = new Scanner(System.in);
 		System.out.println("Please enter each name on a new line:");
 		String[] playerNames = new String[numPlayers];
-		for (int i=0; i<numPlayers; i++){
+		for (int i = 0; i < numPlayers; i++) {
 			playerNames[i] = scan.nextLine();
 		}
-		
+
 		Game g = new Game(playerNames);
-		System.out.println("this game has the following number of player(s): " + g.getNumPlayers());
-		System.out.println("Their names are "+g.getPlayerNames());
+		System.out.println("this game has the following number of player(s): "
+				+ g.getNumPlayers());
+		System.out.println("Their names are " + g.getPlayerNames());
 	}
 }
---
+++ src/view/PartOfTurnPanel.java
@@ -0,0 +1,80 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.Game;
+
+public class PartOfTurnPanel extends JPanel {
+
+	/** */
+	private static final long serialVersionUID = 1L;
+	
+	/** Holds the state of the game */
+	private Game gamestate;
+	
+	/** Holds the player who's turn it is */
+	private JLabel name;
+	
+	/** Holds the part of turn that the player is on */
+	private JLabel part;
+	
+	public PartOfTurnPanel(Game game){
+		gamestate = game;
+		JPanel innerpanel = new JPanel();
+		innerpanel.setLayout(new GridLayout(2,2));
+		
+		JLabel label = new JLabel("Player's turn:");
+		innerpanel.add(label);
+		
+		name = new JLabel(gamestate.getPlayer(gamestate.getCurrentPlayer()+1).getName());
+		innerpanel.add(name);
+		
+		label = new JLabel("Turn Part:");
+		innerpanel.add(label);
+		
+		part = new JLabel(Game.TURN_PARTS[gamestate.getCurrentTurnPart()]);
+		innerpanel.add(part);
+		
+		this.setLayout(new BorderLayout());
+		add(innerpanel, BorderLayout.CENTER);
+		JButton continueBtn = new JButton("Continue");
+		continueBtn.addActionListener(new ContinueListener());
+		add(continueBtn, BorderLayout.SOUTH);
+	}
+	
+	public void updatePanel(){
+		name.setText(gamestate.getPlayer(gamestate.getCurrentPlayer()+1).getName());
+		part.setText(Game.TURN_PARTS[gamestate.getCurrentTurnPart()]);
+	}
+	
+	public static void main(String[] args){
+		String[] names = {"Him", "Her","You","Them"};
+		Game game = new Game(names);
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		PartOfTurnPanel panel = new PartOfTurnPanel(game);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public class ContinueListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			gamestate.nextTurnPart();
+			updatePanel();
+		}
+		
+	}
+
+}
